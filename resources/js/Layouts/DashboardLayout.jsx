0a5b023044f9d15694821acfc4a8b26ad3280684
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-10 07:11:54 -0700

    update

---
+++ resources/js/Layouts/DashboardLayout.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Link, Head } from '@inertiajs/react';
+import { useState } from 'react';
+import Header from '@/Components/Dashboard/Header';
---
+++ resources/js/Pages/Basetemplate.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DashboardLayout from '@/Layouts/DashboardLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Basetemplate() {
+    return (
+        <>
+            <div>
+                Hallo
+            </div>
+        </>
+    )
+}
